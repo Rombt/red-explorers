304d6162c960d3ffe8eed7f1ef5702c56938507c
Rombt <rombtnet@gmail.com>
2024-10-03 14:16:38 +0300

    .

--- app/src/index.php
+++ app/src/index.php
@@ -3,16 +3,15 @@
 
 
 <div class="rmbt-full-width">
-	<div class="rmbt-container">
-		<div class="rmbt-row">
-			<?php get_sidebar() ?>
+   <div class="rmbt-container">
+      <div class="rmbt-row">
+         <?php get_sidebar() ?>
 
-			<main class="rmbt-main">
-				<?php get_template_part( 'template-parts\parts\section-0\section-0', '0' ); ?>
+         <main class="rmbt-main">
 
-			</main>
-		</div>
-	</div>
+         </main>
+      </div>
+   </div>
 </div>
 
 <?php get_template_part( 'template-parts/components/pagination' ); ?>
