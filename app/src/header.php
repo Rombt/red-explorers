c6778d2ded1ecbc40da43b2c19b6feebab773626
Rombt <rombtnet@gmail.com>
2024-10-13 12:23:43 +0300

    ,

--- app/src/header.php
+++ app/src/header.php
@@ -18,25 +18,25 @@
    <div class="rmbt-page-wrap">
 
       <?php if ( is_home() ) {
-			get_template_part( 'template-parts\parts\header\header-blog' );
-		} elseif ( is_single() ) {
-			get_template_part( 'template-parts\parts\header\header', null, [ 
-				'title' => get_the_title(),
-			] );
-		} elseif ( is_tax( 'location' ) ) {
-			get_template_part( 'template-parts\parts\header\header', null, [ 
-				'title' => single_term_title( '', false ),
-			] );
-		} elseif ( is_post_type_archive() ) {
-			get_template_part( 'template-parts\parts\header\header', null, [ 
-				'title' => post_type_archive_title( '', false ),
-				'class' => 'rmbt-archive',
-			] );
-		} elseif ( is_page( 'Destination' ) ) {
-			get_template_part( 'template-parts\parts\header\header', null, [ 
-				'title' => rmbt_get_redux_field( 'rmbt-header_title', 1 ),
-			] );
-		}
-
-
-		?>
+		get_template_part( 'template-parts/parts/header/header-blog' ); 
+	} elseif ( is_single() ) {
+		get_template_part( 'template-parts/parts/header/header', null, [ 
+			'title' => get_the_title(),
+		] );
+	} elseif ( is_tax( 'location' ) ) {
+		get_template_part( 'template-parts/parts/header/header', null, [ 
+			'title' => single_term_title( '', false ),
+		] );
+	} elseif ( is_post_type_archive() ) {
+		get_template_part( 'template-parts/parts/header/header', null, [ 
+			'title' => post_type_archive_title( '', false ),
+			'class' => 'rmbt-archive',
+		] );
+	} elseif ( is_page( 'Destination' ) ) {
+		get_template_part( 'template-parts/parts/header/header', null, [ 
+			'title' => rmbt_get_redux_field( 'rmbt-header_title', 1 ),
+		] );
+	} else {
+		get_template_part( 'template-parts/parts/header/header-default' );
+	}
+	?>
