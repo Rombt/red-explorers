5773aba39797d29bb7023b50acfc9a5a54974990
Rombt <rombtnet@gmail.com>
2024-10-13 12:47:35 +0300

    .

--- app/src/index.php
+++ app/src/index.php
@@ -3,14 +3,14 @@
 
 
 <div class="rmbt-full-width">
-   <div class="rmbt-container">
-      <div class="rmbt-row">
-         <?php get_sidebar() ?>
+	<div class="rmbt-container">
+		<div class="rmbt-row">
+			<?php get_sidebar() ?>
 
-         <main class="rmbt-main">
+			<main class="rmbt-main">
 
 
-            <?php
+				<?php
 				$i_post = 0;
 				if ( have_posts() ) {
 					while ( have_posts() ) :
@@ -35,7 +35,6 @@
 							$image = wp_get_attachment_image( 532, 'rmbt_post-img' );
 						}
 
-
 						get_template_part( 'template-parts/components/card/card', null, [ 
 							'class' => $class,
 							'tag-img' => $image,
@@ -55,9 +54,9 @@
 				?>
 
 
-         </main>
-      </div>
-   </div>
+			</main>
+		</div>
+	</div>
 </div>
 
 <?php //  get_template_part( 'template-parts/components/pagination' ); ?>
